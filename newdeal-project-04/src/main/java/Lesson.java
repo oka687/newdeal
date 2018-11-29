c0e63a442fb5fafbf5db667066e2ac5fbea24374
oka687 <oka687@naver.com>
2018-11-29 10:48:18 +0900

    'v04.1'

---
+++ newdeal-project-04/src/main/java/Lesson.java
@@ -0,0 +1,17 @@
+
+
+import java.sql.Date;
+
+public class Lesson {
+//새로운 종류의 메모리를 구성하는 설계도
+//새 데이터 타입
+	int no;
+	String title;
+	String contents;
+	Date startDate;
+	Date endDate;
+	int totalHours;
+	int dayHours;
+	//인스턴스 필드. 데이터를 개별적을 다뤄야 하는 경우에는 인스턴스 변수로 만들어야 한다.
+	
+}
